4b01cc489455e0549245611eb7aacf5e9cc58bdf
Leandro Diniz Silva <leandrodinizsilva@users.noreply.github.com>
2021-08-06 20:21:28 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -3,7 +3,7 @@ public class Movie {
     public static final int  CHILDRENS = 2;
     public static final int  REGULAR = 0;
     public static final int  NEW_RELEASE = 1;
-  
+
     private String _title;
     private Price _price;
 
@@ -37,27 +37,9 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        //determine amounts for each line
-        double thisAmount = 0;
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return _price.getCharge(daysRented);
     }
 
-    
    public int getFrequentRenterPoints(int daysRented) {
         // add bonus for a two day new release rental
         int result = 0;
