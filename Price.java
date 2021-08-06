1f2580b2af921c912fbd38a28f8b3c0ad20e3f4e
Leandro Diniz Silva <leandrodinizsilva@users.noreply.github.com>
2021-08-06 20:28:41 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -2,10 +2,6 @@ public abstract class Price {
     public abstract int getPriceCode();
     public abstract double getCharge(int daysRented);
     public int getFrequentRenterPoints(int daysRented) {
-        int result = 0;
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-        result = 1;
-        }
-        return result;
+        return 1;
     }
 }
